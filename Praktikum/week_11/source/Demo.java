90aeac788d4f46f62845b0bb4dd19038a95bd935
rankadian <dianthafafian@gmail.com>
2024-11-12 20:31:50 +0700

    source js 11 interface

---
+++ Praktikum/week_11/source/Demo.java
@@ -0,0 +1,13 @@
+package week_11.source;
+
+import java.util.ArrayList;
+
+public class Demo {
+    Dosen dosen1 = new Dosen("19940201", "Widia, S.Kom. M.Kom", "199402");
+    Dosen dosen2 = new Dosen("19700105", "Muhammad, S.T, M.T", "197001");
+
+    TenagaKependidikan tendik1 = new TenagaKependidikan("19750301", "Aida, A.Md.", "Tenaga Administrasi");
+    TenagaKependidikan tendik2 = new TenagaKependidikan("19650304", "Rika, S.T.", "Tenaga Laboratorium");
+
+    ArrayList<Pegawai> daftarPegawai = new ArrayList<Pegawai>();
+}
